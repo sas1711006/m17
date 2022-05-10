8b094039f59f5aa956a9617b16ed29840ad7afe7
sas1711006 <105028870+sas1711006@users.noreply.github.com>
2022-05-10 15:02:16 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    if (input.lightLevel() >= 5) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+})
